2142025df9e5dc3683d84678749713af52c69361
mshsvnv <mshsvnv@mail.ru>
2024-10-23 21:24:49 +0300

    some fixes

--- src/e2e/utils.go
+++ src/e2e/utils.go
@@ -2,13 +2,11 @@ package e2e
 
 import (
 	"context"
-	"time"
 
 	"src/internal/model"
 	"src/internal/repository"
 	mypostgres "src/internal/repository/postgres"
 	"src/pkg/storage/postgres"
-	"src/pkg/utils"
 )
 
 const connString = "postgresql://postgres:admin@localhost:5434/tests"
@@ -22,11 +20,7 @@ func NewTestStorage() (*postgres.Postgres, map[string]int) {
 	}
 
 	ids = map[string]int{}
-	ids["userID"] = initUserRepository(mypostgres.NewUserRepository(conn))
 	ids["racketID"] = initRacketRepository(mypostgres.NewRacketRepository(conn))
-	ids["orderID"] = initOrderRepository(mypostgres.NewOrderRepository(conn))
-	ids["cartID"] = initCartRepository(mypostgres.NewCartRepository(conn))
-
 	return conn, ids
 }
 
@@ -35,44 +29,10 @@ func DropTestStorage(testDB *postgres.Postgres) {
 		testDB.Close()
 	}()
 
-	err := mypostgres.NewUserRepository(testDB).Delete(context.TODO(), ids["userID"])
+	err := mypostgres.NewRacketRepository(testDB).Delete(context.TODO(), ids["racketID"])
 	if err != nil {
 		panic(err)
 	}
-
-	err = mypostgres.NewCartRepository(testDB).Delete(context.TODO(), ids["cartID"])
-	if err != nil {
-		panic(err)
-	}
-
-	err = mypostgres.NewRacketRepository(testDB).Delete(context.TODO(), ids["racketID"])
-	if err != nil {
-		panic(err)
-	}
-
-	err = mypostgres.NewCartRepository(testDB).Delete(context.TODO(), ids["racketID"])
-	if err != nil {
-		panic(err)
-	}
-}
-
-func initUserRepository(repo repository.IUserRepository) int {
-
-	user := &model.User{
-		Name:     "Peter",
-		Surname:  "Petrov",
-		Email:    "peter@mail.ru",
-		Role:     model.UserRoleCustomer,
-		Password: utils.HashAndSalt([]byte("peter")),
-	}
-
-	err := repo.Create(context.TODO(), user)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return user.ID
 }
 
 func initRacketRepository(repo repository.IRacketRepository) int {
@@ -94,53 +54,4 @@ func initRacketRepository(repo repository.IRacketRepository) int {
 	}
 
 	return racket.ID
-}
-
-func initOrderRepository(repo repository.IOrderRepository) int {
-
-	tm, _ := time.Parse(time.RFC3339, "2006-01-02T15:07:00Z")
-	order := &model.Order{
-		UserID:        ids["userID"],
-		CreationDate:  tm,
-		Address:       "Moscow",
-		RecepientName: "Ivan Ivanov",
-		Status:        model.OrderStatusInProgress,
-		Lines: []*model.OrderLine{
-			{
-				RacketID: ids["racketID"],
-				Quantity: 1,
-			},
-		},
-	}
-
-	err := repo.Create(context.TODO(), order)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return order.ID
-}
-
-func initCartRepository(repo repository.ICartRepository) int {
-
-	cart := &model.Cart{
-		UserID:     ids["userID"],
-		TotalPrice: 300,
-		Quantity:   1,
-		Lines: []*model.CartLine{
-			{
-				RacketID: ids["racketID"],
-				Quantity: 1,
-			},
-		},
-	}
-
-	err := repo.Create(context.TODO(), cart)
-
-	if err != nil {
-		panic(err)
-	}
-
-	return cart.UserID
 }
