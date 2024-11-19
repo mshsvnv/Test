cbcab9f45b334d66b13efc5e646b74008b21938e
mshsvnv <mshsvnv@mail.ru>
2024-11-19 23:54:17 +0300

    -_-

--- src/e2e/login_test.go
+++ src/e2e/login_test.go
@@ -16,7 +16,7 @@ func TestLogin(t *testing.T) {
 
 	clnt := &http.Client{}
 	expectLogin = httpexpect.WithConfig(httpexpect.Config{
-		BaseURL:  "http://localhost:8085/api/v2/auth",
+		BaseURL:  "http://localhost:7777/api/v2/auth",
 		Client:   clnt,
 		Reporter: httpexpect.NewRequireReporter(nil),
 	})
